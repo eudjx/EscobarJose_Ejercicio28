b57fde915cc6eabb2ecb854c4f22dd5518d2d146
eudjx <56890938+eudjx@users.noreply.github.com>
2019-11-14 12:01:10 -0500

    Update Ex28.cpp

--- Ex28.cpp
+++ Ex28.cpp
@@ -2,13 +2,7 @@
 #include<cmath>
 #include<stdio.h> 
 
-// Código ayuda tomado de https://www.geeksforgeeks.org/runge-kutta-4th-order-method-solve-differential-equation/
 
-// double vy(double t, double n)
-// {
-// 	for (int i=0; i<)
-	
-// }
 int N=20;
 int *norma=new int [N];
 double dx2dt2(double t, double x)
@@ -19,8 +13,6 @@ double dx2dt2(double t, double x)
 	double vy=15;
 	int n=2;
 	double g=9.8;
-    //double norm_v=sqrt(pow(vx,2)+pow(vy,2));
-	//t *norma=new int [N];
 	for (int i=0; i<20; i++)
 	{
 		norma[i]=sqrt(pow(vx,2)+pow(vy-g*i,2));
@@ -40,8 +32,7 @@ double dy2dt2(double t, double y)
 	double vy=15;
 	int n=2;
 	double g=9.8;
-    //double norm_v=sqrt(pow(vx,2)+pow(vy,2));
-	//t *norma=new int [N];
+    
 	for (int i=0; i<20; i++)
 	{
 		norma[i]=sqrt(pow(vx,2)+pow(vy-g*i,2));
@@ -55,14 +46,13 @@ double dy2dt2(double t, double y)
 
 
 
-int main(/*double t0, double x0, double t, double h*/)
+int main()
 {
-	//double *puntero;
+	
 	double t0=0, x=1, t=2, h=0.1, y=1;
 	int N=(int)((t-t0)/h);
     double k1, k2, k3 , k4;
     double v0y=15;
-	//std::cout<<"El valor de x es:  "<<std::endl;
     for (int i=0; i<20; i++)
     {
 		double g=9.8;
@@ -92,10 +82,3 @@ int main(/*double t0, double x0, double t, double h*/)
     }
     return 0;
 }
-
-// int main()
-// {
-//  double x0=0, y=1, x=2, h=0.1;
-//     std::cout<<"EL valor de x en t=  "<<i<<"es:  "<<rk4(x0, y, x, h)<<std::endl;
-    
-//     return 0;
